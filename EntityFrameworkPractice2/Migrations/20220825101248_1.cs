c89dc07f7f0958b7030eef8bfb9e1154bd0c2f40
unknown <cogdotnet1687@iiht.tech>
2022-08-26 13:11:03 +0530

    Entity Framework Api ptactice

---
+++ EntityFrameworkPractice2/Migrations/20220825101248_1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    RollNo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.RollNo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ EntityFrameworkPractice2/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int RollNo { get; set; }
+        public int Name { get;set; }
+        public string Address { get;set; }
+        
+    }
+}
