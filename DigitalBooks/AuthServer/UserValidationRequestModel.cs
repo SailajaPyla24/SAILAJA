9b84c0a27b8481f65bf85a685f6e11f25f42b098
unknown <cogdotnet1687@iiht.tech>
2022-09-09 09:59:49 +0530

    Digital Books Api project

---
+++ DigitalBooks/AuthServer/UserValidationRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.DatabaseEntity;
+
+internal class UserValidationRequestModel
+{
+    private readonly DIGITALBOOKSContext context = new DIGITALBOOKSContext();
+
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public bool IsValidate(string username, string password)
+    {
+        if (context.UserTables.Any(x => x.UserName == username && x.Password ==
+        AuthServer.EncryptDecrypt.EncodePasswordToBase64(password)))
+           { 
+                return true;
+        }
+        else
+            return false;
+    }
+}
